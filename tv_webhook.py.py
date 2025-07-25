7206baef8688550ce7bfeb9c47bd3bfc841c3a3e
BEHEAD83955 <BEHEAD83955@GMAIL.com>
2025-07-25 20:13:13 +0800

    first commit

---
+++ tv_webhook.py.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+BOT_TOKEN = '7929780148:AAEKw3t9XUQdc-LkxK2J9tCWwbxqMtahjoU'
+CHAT_ID = '7407074098'
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.json
+    message = data.get('message', 'TradingView 訊號來了！')
+    
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {'chat_id': CHAT_ID, 'text': message}
+    
+    requests.post(url, data=payload)
+    return 'ok'
+
+if __name__ == '__main__':
+    app.run()
